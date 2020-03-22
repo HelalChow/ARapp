2373296c739e1d5d939065340b2386eb461a9407
hchowdhury3273 <hc2324@nyu.edu>
2020-03-22 12:58:32 -0400

    update

--- augmentedReality/augmentedReality/ViewController.swift
+++ augmentedReality/augmentedReality/ViewController.swift
@@ -19,14 +19,53 @@ class ViewController: UIViewController{
         // Shows world origin and feature points
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
         
-        
-
-    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
-          
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        // Make the node a box
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        // Add reflection to the node
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        // Give the node a color
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        // Gives the node a position from the world origin
+        node.position = SCNVector3(0,0,-0.5)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    // Reset all nodes
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    // Pause session to stop keeping track of position
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
         }
-        
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    //Gives random number
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+    
+    
+    
+    
+
+//    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+//
+//        }
+//
        
         
     }
